f592701f897e53e2b30abb832d9b1e8003600513
Nadav Mary <nadavmary@gmail.com>
2019-04-19 14:32:33 +0300

    add play on click

--- 1-drum-kit/app.js
+++ 1-drum-kit/app.js
@@ -3,18 +3,29 @@ function removeTransition(e) {
     this.classList.remove('play');
 }
 
+function onBoxClicked() {
+    this.classList.add('play');
+    const dataKey = this.getAttribute('data-key');
+    const selectedAudio = document.querySelector(`audio[data-key="${dataKey}"]`);
+    selectedAudio.currentTime = 0;
+    selectedAudio.play();
+}
+
 function addTransitionendEventListener() {
     const boxes = document.querySelectorAll('.drum-kit__component__box');
-    boxes.forEach((box) => box.addEventListener('transitionend', removeTransition))
+    boxes.forEach((box) => {
+        box.addEventListener('transitionend', removeTransition);
+        box.addEventListener('click', onBoxClicked)
+    })
 }
 
 function playByKeyCode(keyCode) {
     const selectedInstrument = document.querySelector(`.drum-kit__component__box[data-key="${keyCode}"`);
-    const selectAudio = document.querySelector(`audio[data-key="${keyCode}"]`);
-    if(!selectedInstrument || !selectAudio) return;
+    const selectedAudio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    if(!selectedInstrument || !selectedAudio) return;
     selectedInstrument.classList.add('play');
-    selectAudio.currentTime = 0;
-    selectAudio.play();
+    selectedAudio.currentTime = 0;
+    selectedAudio.play();
 }
 
 addTransitionendEventListener();
